c6d370a4ae997446a72272a6453bc1cda36965ca
David Atanda <ololadedavid15@gmail.com>
2023-09-11 14:10:01 +0100

    working on template

---
+++ templates.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+        body {            
+            background-color: #515100 !important;
+        }
+        .cursor-pointer {
+            cursor: pointer;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Docify | Templates</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>        
+</head>
+<body>
+    <div class="container py-5">
+        <p class="text-center text-warning fs-1">
+            Choose a template
+        </p>
+    </div>
+</body>
+</html>
