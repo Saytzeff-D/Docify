39f99ad9f7bf675166305840ea9200f8d62113b6
David Atanda <ololadedavid15@gmail.com>
2023-09-11 16:05:07 +0100

    working on the model

---
+++ rent.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+        body {            
+            background-color: #515100 !important;
+        }
+        .cursor-pointer {
+            cursor: pointer;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Docify | Rental Agreement</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>        
+</head>
+<body>
+    <div class="container py-5">
+        <div>
+            <p class="text-warning fs-4">Room Rental Agreement</p>
+        </div>        
+        <div class="w-100 mt-3">
+            <div class="progress" style="height: 20px">
+                <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning fw-bold" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 25%">75%</div>
+            </div>
+        </div>      
+        <div class="row mx-auto w-100">
+            <div class="col-md-4"></div>
+            <div class="col-md-8 py-2">
+                <div class="bg-warning overflow-auto">
+                    ROOM RENTAL AGREEMENT
+
+                    Parties
+
+                    This Room Rental Agreement ("Agreement") is entered into in [City], on [Date], between:
+
+                    LANDLORD (Property Owner):
+                    Landlord's Name: [Landlord's Name]
+                    Landlord's Address: [Landlord's Address]
+                    Landlord's Tax Identification Number: [Landlord's NIF/CIF]
+
+                    TENANT (Renter):
+                    Tenant's Name: [Tenant's Name]
+                    Tenant's Address: [Tenant's Address]
+                    Tenant's Tax Identification Number: [Tenant's NIF/CIF]
+
+                    Purpose of the Agreement
+
+                    The Landlord leases a room in the dwelling located at [Dwelling Address] ("Dwelling"), under the following conditions:
+
+                    Duration of the Lease
+
+                    The lease term shall be [Number of months/years] commencing on [Start Date] and ending on [End Date].
+
+                    Rent
+
+                    The Tenant agrees to pay a monthly rent of [Amount] euros (€), to be paid within the first [Day] days of each month. The agreed payment method is [Payment Method].
+
+                    Security Deposit
+
+                    The Tenant shall provide the Landlord with a security deposit equivalent to [Amount] euros (€) at the time of signing this agreement. The deposit will be refunded at the end of the contract, deducting costs for damages or unpaid rent.
+
+                    Use of Common Areas
+
+                    The Tenant shall have the right to use the common areas of the Dwelling, including [Specify available common areas such as kitchen, bathroom, living room, etc.].
+
+                    Landlord's Obligations
+
+                    The Landlord agrees to maintain the Dwelling in suitable conditions for habitation and to make necessary repairs to ensure proper functioning of facilities and services.
+
+                    Tenant's Obligations
+
+                    The Tenant agrees to use the room and common areas of the Dwelling reasonably and respectfully of the rights of other tenants or individuals living in the Dwelling.
+
+                    Termination of the Agreement
+
+                    Either party may terminate this Agreement by providing written notice to the other party with [Number of days] days' notice.
+
+                    Applicable Law and Jurisdiction
+
+                    This Agreement shall be governed and interpreted in accordance with the laws of [Applicable Jurisdiction].
+
+                    Any dispute related to this Agreement shall be subject to the exclusive jurisdiction of the courts of [City or Jurisdiction].
+
+                    Signatures of the Parties
+
+                    The parties have executed this Room Rental Agreement on the date mentioned above.
+
+                    By the Landlord:
+
+                    Signature: _______________________________
+                    Printed Name: __________________________
+                    Date: _______________________________
+
+                    By the Tenant:
+
+                    Signature: _______________________________
+                    Printed Name: __________________________
+                    Date: _______________________________
+                </div>
+            </div>
+        </div>  
+    </div>
+</body>
+</html>
