417d4f632153a4caf5df6011df7bdf6687b03e37
David Atanda <ololadedavid15@gmail.com>
2023-09-13 12:48:04 +0100

    completed the ui

--- rent.php
+++ rent.php
@@ -8,6 +8,9 @@
         .cursor-pointer {
             cursor: pointer;
         }
+        .doc-page {
+            
+        }
     </style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,6 +19,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>        
 </head>
 <body>
+    <?php
+        $city = 'Ogbomoso'
+    ?>
     <div class="container py-5">
         <div>
             <p class="text-warning fs-4">Room Rental Agreement</p>
@@ -26,81 +32,98 @@
             </div>
         </div>      
         <div class="row mx-auto w-100">
-            <div class="col-md-4"></div>
-            <div class="col-md-8 py-2">
-                <div class="bg-warning overflow-auto">
-                    ROOM RENTAL AGREEMENT
+            <div class="col-md-4 py-2">
+                <div class="bg-warning p-4">
+                    <p class="text-dark fs-5 fw-bold">
+                        You are from which city?
+                    </p>
+                    <input onInput="clickMe(event)" class="form-control bg-warning" /> 
+                    <div class="pt-3">
+                        <button class="btn btn-dark fw-bold" >Prev</button>
+                        <button class="btn btn-dark fw-bold" >Next</button>
+                    </div>                                       
+                </div>
+            </div>
+            <div class="col-md-8 py-2 doc-page">
+                <div class="bg-warning overflow-auto p-4">
+                    <p>ROOM RENTAL AGREEMENT</p>
 
-                    Parties
+                    <p>Parties</p>
 
-                    This Room Rental Agreement ("Agreement") is entered into in [City], on [Date], between:
+                    <p>This Room Rental Agreement ("Agreement") is entered into in <i><?php echo $city ?></i>, on [Date], between:</p>
 
-                    LANDLORD (Property Owner):
-                    Landlord's Name: [Landlord's Name]
-                    Landlord's Address: [Landlord's Address]
-                    Landlord's Tax Identification Number: [Landlord's NIF/CIF]
+                    <p>LANDLORD (Property Owner):</p>
+                    <p>Landlord's Name: [Landlord's Name]</p>
+                    <p>Landlord's Address: [Landlord's Address]</p>
+                    <p>Landlord's Tax Identification Number: [Landlord's NIF/CIF]</p>
 
-                    TENANT (Renter):
-                    Tenant's Name: [Tenant's Name]
-                    Tenant's Address: [Tenant's Address]
-                    Tenant's Tax Identification Number: [Tenant's NIF/CIF]
+                    <p>TENANT (Renter):</p>
+                    <p>Tenant's Name: [Tenant's Name]</p>
+                    <p>Tenant's Address: [Tenant's Address]</p>
+                    <p>Tenant's Tax Identification Number: [Tenant's NIF/CIF]</p>
 
-                    Purpose of the Agreement
+                    <p>Purpose of the Agreement</p>
 
-                    The Landlord leases a room in the dwelling located at [Dwelling Address] ("Dwelling"), under the following conditions:
+                    <p>The Landlord leases a room in the dwelling located at [Dwelling Address] ("Dwelling"), under the following conditions:</p>
 
-                    Duration of the Lease
+                    <p>Duration of the Lease</p>
 
-                    The lease term shall be [Number of months/years] commencing on [Start Date] and ending on [End Date].
+                    <p>The lease term shall be [Number of months/years] commencing on [Start Date] and ending on [End Date].</p>
 
-                    Rent
+                    <p>Rent</p>
 
-                    The Tenant agrees to pay a monthly rent of [Amount] euros (€), to be paid within the first [Day] days of each month. The agreed payment method is [Payment Method].
+                    <p>The Tenant agrees to pay a monthly rent of [Amount] euros (€), to be paid within the first [Day] days of each month. The agreed payment method is [Payment Method].</p>
 
-                    Security Deposit
+                    <p>Security Deposit</p>
 
-                    The Tenant shall provide the Landlord with a security deposit equivalent to [Amount] euros (€) at the time of signing this agreement. The deposit will be refunded at the end of the contract, deducting costs for damages or unpaid rent.
+                    <p>The Tenant shall provide the Landlord with a security deposit equivalent to [Amount] euros (€) at the time of signing this agreement. The deposit will be refunded at the end of the contract, deducting costs for damages or unpaid rent.</p>
 
-                    Use of Common Areas
+                    <p>Use of Common Areas</p>
 
-                    The Tenant shall have the right to use the common areas of the Dwelling, including [Specify available common areas such as kitchen, bathroom, living room, etc.].
+                    <p>The Tenant shall have the right to use the common areas of the Dwelling, including [Specify available common areas such as kitchen, bathroom, living room, etc.].</p>
 
-                    Landlord's Obligations
+                    <p>Landlord's Obligations</p>
 
-                    The Landlord agrees to maintain the Dwelling in suitable conditions for habitation and to make necessary repairs to ensure proper functioning of facilities and services.
+                    <p>The Landlord agrees to maintain the Dwelling in suitable conditions for habitation and to make necessary repairs to ensure proper functioning of facilities and services.</p>
 
-                    Tenant's Obligations
+                    <p>Tenant's Obligations</p>
 
-                    The Tenant agrees to use the room and common areas of the Dwelling reasonably and respectfully of the rights of other tenants or individuals living in the Dwelling.
+                    <p>The Tenant agrees to use the room and common areas of the Dwelling reasonably and respectfully of the rights of other tenants or individuals living in the Dwelling.</p>
 
-                    Termination of the Agreement
+                    <p>Termination of the Agreement</p>
 
-                    Either party may terminate this Agreement by providing written notice to the other party with [Number of days] days' notice.
+                    <p>Either party may terminate this Agreement by providing written notice to the other party with [Number of days] days' notice.</p>
 
-                    Applicable Law and Jurisdiction
+                    <p>Applicable Law and Jurisdiction</p>
 
-                    This Agreement shall be governed and interpreted in accordance with the laws of [Applicable Jurisdiction].
+                    <p>This Agreement shall be governed and interpreted in accordance with the laws of [Applicable Jurisdiction].</p>
 
-                    Any dispute related to this Agreement shall be subject to the exclusive jurisdiction of the courts of [City or Jurisdiction].
+                    <p>Any dispute related to this Agreement shall be subject to the exclusive jurisdiction of the courts of [City or Jurisdiction].</p>
 
-                    Signatures of the Parties
+                    <p>Signatures of the Parties</p>
 
-                    The parties have executed this Room Rental Agreement on the date mentioned above.
+                    <p>The parties have executed this Room Rental Agreement on the date mentioned above.</p>
 
-                    By the Landlord:
+                    <p>By the Landlord:</p>
 
-                    Signature: _______________________________
-                    Printed Name: __________________________
-                    Date: _______________________________
+                    <p>Signature: _______________________________</p>
+                    <p>Printed Name: __________________________</p>
+                    <p>Date: _______________________________</p>
 
-                    By the Tenant:
+                    <p>By the Tenant:</p>
 
-                    Signature: _______________________________
-                    Printed Name: __________________________
-                    Date: _______________________________
+                    <p>Signature: _______________________________</p>
+                    <p>Printed Name: __________________________</p>
+                    <p>Date: _______________________________</p>
                 </div>
             </div>
         </div>  
     </div>
+
+    <script type="text/javascript" >
+        const clickMe = (e)=>{
+            console.log(e.target.value)
+        }
+    </script>
 </body>
 </html>
